0bcf499236077b424c6c40ac9da93a727043b27e
Ties Bloemen <t.t.j.bloemen@student.tudelft.nl>
2024-05-13 22:25:46 +0200

    Add constants for daregram loss function and rir preprocessing

--- constants.py
+++ constants.py
@@ -10,6 +10,13 @@ LOW_DELTA = 0.5
 # How many iterations should this low change be after each other?
 SAME_DELTA_EPOCHS = 10
 
+THRESHOLD = 0.9
+TRADEOFF_ANGLE = 0.05
+TRADEOFF_SCALE = 0.001
+
+SAMPLERATE = 16000
+RIR_DURATION = 1.5
+
 
 class NnStage(Enum):
     TRAINING = "training"
